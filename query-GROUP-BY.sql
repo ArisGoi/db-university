fad8f4415e7c5c03ca3c96840735e5c2c6a8cc9f
ArisGoi <aris.goi97@gmail.com>
2021-10-28 17:20:02 +0200

    add GROUP 3, 4

--- query-GROUP-BY.sql
+++ query-GROUP-BY.sql
@@ -9,6 +9,17 @@ FROM `teachers`
 GROUP BY `office_address`
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
-
+SELECT `exams`.`id`, `courses`.`name`, ROUND(AVG(`exam_student`.`vote`), 1) AS 'media_voti'
+FROM `exams`
+JOIN `exam_student`
+ON `exam_student`.`exam_id` = `exams`.`id`
+JOIN `courses`
+ON `courses`.`id` = `exams`.`course_id`
+GROUP BY `exams`.`id`
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+SELECT `departments`.`name` AS 'department_name', COUNT(`degrees`.`name`) AS 'degree_count'
+FROM `departments`
+JOIN `degrees`
+ON `degrees`.`department_id` = `departments`.`id`
+GROUP BY `departments`.`name`
