fcd0c4c4721f1df4bc24677b12b13d3dda9296e2
ArisGoi <aris.goi97@gmail.com>
2021-10-28 16:25:25 +0200

    add JOIN 3, 6

--- query-JOIN.sql
+++ query-JOIN.sql
@@ -14,6 +14,13 @@ ON `degrees`.`department_id` = `departments`.`id`
 WHERE `departments`.`id` = 7
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT `teachers`.`id` AS 'ID_insegnante', `teachers`.`name` AS 'nome_insegnante', `teachers`.`surname` AS 'cognome_insegnante', `courses`.`id` AS 'ID_corso', `courses`.`name` AS 'nome_corso', `courses`.`period`, `courses`.`cfu`
+FROM `teachers`
+JOIN `course_teacher`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+JOIN `courses`
+ON `course_teacher`.`course_id` = `courses`.`id`
+WHERE `teachers`.`id` = '44'
 
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
@@ -23,6 +30,17 @@ WHERE `departments`.`id` = 7
 
 
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+SELECT `teachers`.`id` AS 'ID_insegnante', `teachers`.`name` AS 'nome_insegnante', `teachers`.`surname` AS 'cognome_insegnante', `departments`.`name` AS 'nome_dipartimento'
+FROM `teachers`
+JOIN `course_teacher`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+JOIN `courses`
+ON `course_teacher`.`course_id` = `courses`.`id`
+JOIN `degrees`
+ON `courses`.`degree_id` = `degrees`.`id`
+JOIN `departments`
+ON `degrees`.`department_id` = `departments`.`id`
+WHERE `departments`.`name` = 'Dipartimento di Matematica'
 
 
 -- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
